e398138bf3b87e3dd5950ad3fee5428c6ee7e58a
MURILO ANTUNES CAMPOS DA ROCHA <25203706@cfp127aluno.local>
2026-03-11 10:41:29 -0300

    desafio 2 concluido

--- 02-calculadora-desconto/main.js
+++ 02-calculadora-desconto/main.js
@@ -8,7 +8,23 @@ function calcularDesconto() {
     console.log(n1.value)
     console.log(n2.value)
 
-    resultado.value = n1.value - ((n1.value * 100) / n2.value )
+    let conta = n1.value - ((n1.value / 100) * n2.value)
 
+    removerClasse()
+    if(n2.value < 5){
+        resultado.classList.add('baixo')
+    }else if(n2.value >= 5 && n2.value< 10 ){
+        resultado.classList.add('medio')
+    }else{
+        resultado.classList.add('alto')
+    }
+
+    resultado.textContent = conta.toFixed(2)
+    n1.focus()
     console.log(resultado.value)
+}
+
+function removerClasse(){
+    const resultado = document.getElementById('resultado')
+    resultado.classList.remove('baixo', 'medio', 'alto')
 }
