d57cf6d21d507da6dde4793d443f21e66d3b71bf
MURILO ANTUNES CAMPOS DA ROCHA <25203706@cfp127aluno.local>
2026-03-17 10:16:25 -0300

    validação da atividade 2 feita

--- 02-calculadora-desconto/main.js
+++ 02-calculadora-desconto/main.js
@@ -5,26 +5,54 @@ function calcularDesconto() {
     const n2 = document.getElementById('n2')
     const resultado = document.getElementById('resultado')
 
-    console.log(n1.value)
-    console.log(n2.value)
+    const validacao = validarNumero(n1.value, n2.value)
 
-    let conta = n1.value - ((n1.value / 100) * n2.value)
+    if(validacao){
+        let conta = n1.value - ((n1.value / 100) * n2.value)
 
-    removerClasse()
-    if(n2.value < 5){
-        resultado.classList.add('baixo')
-    }else if(n2.value >= 5 && n2.value< 10 ){
-        resultado.classList.add('medio')
-    }else{
-        resultado.classList.add('alto')
+        removerClasse()
+        if(n2.value < 5){
+            resultado.classList.add('baixo')
+        }else if(n2.value >= 5 && n2.value< 10 ){
+            resultado.classList.add('medio')
+        }else{
+            resultado.classList.add('alto')
+        }
+    
+        resultado.textContent = conta.toFixed(2)
+        n1.focus()
+        console.log(resultado.value)
     }
-
-    resultado.textContent = conta.toFixed(2)
-    n1.focus()
-    console.log(resultado.value)
 }
 
 function removerClasse(){
     const resultado = document.getElementById('resultado')
     resultado.classList.remove('baixo', 'medio', 'alto')
+}
+
+function validarVazio(item){
+    const itemFormat = String(item)
+    console.log(itemFormat)
+    if(itemFormat.trim() === ""){
+        
+        return false
+    }else{
+        return true
+    }
+}
+
+function validarNumero(n1, n2){
+    if(validarVazio(n1) && validarVazio(n2)){
+        let n1Format = parseInt(n1)
+        let n2Format = parseInt(n2)
+        if(isNaN(n1Format) || isNaN(n2Format)){
+            alert('Preço ou Desconto não é um número')
+            return false
+        }else{
+            return true
+        }
+    }else{
+        alert('Preço ou Desconto está vazio')
+        return false
+    }
 }
