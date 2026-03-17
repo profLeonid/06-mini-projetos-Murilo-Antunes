d749f6a8c24647617e67dd41141ad6de290c8733
MURILO ANTUNES CAMPOS DA ROCHA <25203706@cfp127aluno.local>
2026-03-17 10:13:28 -0300

    validacao da atividade 3 funcionando

--- 03-calculadora-media/main.js
+++ 03-calculadora-media/main.js
@@ -6,24 +6,70 @@ const resultado = document.getElementById('resultado')
 
 
 function calcularMedia(){
-    let conta = (Number(n1.value) + Number(n2.value) + Number(n3.value)) / 3
+    const validacaoNumero = validarNumero(n1.value, n2.value, n3.value)
+    const validacaoMaximo = numeroMaximo(0, 10, n1.value, n2.value, n3.value)
 
-    removerClasse()
-    if(conta >= 7){
-        resultado.textContent = 'Aprovado'
-        resultado.classList.add('aprovado')
-    }else if(conta < 5){
-        resultado.textContent = 'Reprovado'
-        resultado.classList.add('reprovado')
-    }else{
-        resultado.textContent = 'Recuperação'
-        resultado.classList.add('recuperacao')
+    if(validacaoNumero && validacaoMaximo){
+        let conta = (Number(n1.value) + Number(n2.value) + Number(n3.value)) / 3
+
+        removerClasse()
+        if(conta >= 7){
+            resultado.textContent = 'Aprovado'
+            resultado.classList.add('aprovado')
+        }else if(conta < 5){
+            resultado.textContent = 'Reprovado'
+            resultado.classList.add('reprovado')
+        }else{
+            resultado.textContent = 'Recuperação'
+            resultado.classList.add('recuperacao')
+        }
+    
+        n1.focus()
     }
-
-    n1.focus()
 }
 
 function removerClasse(){
     const resultado = document.getElementById('resultado')
     resultado.classList.remove('aprovado', 'reprovado', 'recuperacao')
+}
+
+function validarVazio(item){
+    const itemFormat = String(item)
+    console.log(itemFormat)
+    if(itemFormat.trim() === ""){
+        
+        return false
+    }else{
+        return true
+    }
+}
+
+function validarNumero(n1, n2, n3){
+    if(validarVazio(n1) && validarVazio(n2) && validarVazio(n3)){
+        let n1Format = parseInt(n1)
+        let n2Format = parseInt(n2)
+        let n3Format = parseInt(n3)
+        if(isNaN(n1Format) || isNaN(n2Format) || isNaN(n3Format)){
+            alert('Alguma nota não é um número')
+            return false
+        }else{
+            return true
+        }
+    }else{
+        alert('Alguma nota está vazia')
+        return false
+    }
+}
+
+function numeroMaximo(n1, n2, x, y, z){
+    if(validarNumero(n1, n2, x)){
+        if(x >= n1 && x <= n2 && y >= n1 && y <= n2 && z >= n1 && z <= n2){
+            return true
+        }else{
+            alert('Alguma nota não está entre os números permitidos')
+            return false
+        }
+    }else{
+        return false
+    }
 }
